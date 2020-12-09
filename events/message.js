054c40d607eb11a54b2e5d2dc451297abaced3ff
GrahamSH <64214252+GrahamSH-LLK@users.noreply.github.com>
2020-12-09 12:53:18 -0500

    Gtg command

--- events/message.js
+++ events/message.js
@@ -1,47 +1,65 @@
 module.exports = (client, message) => {
-  const escapeRegex = str => str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-  const Discord = require("discord.js")
-  var randomSentence = require('random-sentence');
-  var Sentencer = require('sentencer');
-  const prefix = 'g!';
-  const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|${escapeRegex(prefix)})\\s*`);
+  const escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+  const Discord = require("discord.js");
+  var randomSentence = require("random-sentence");
+  var Sentencer = require("sentencer");
+  const prefix = "g!";
+  const prefixRegex = new RegExp(
+    `^(<@!?${client.user.id}>|${escapeRegex(prefix)})\\s*`
+  );
   if (!prefixRegex.test(message.content)) return;
   const [, matchedPrefix] = message.content.match(prefixRegex);
   const args = message.content.slice(matchedPrefix.length).trim().split(/ +/);
   const command = args.shift().toLowerCase();
-console.log(message.content + ' -' + message.author)
-const Help = new Discord.RichEmbed()
-	.setColor('#0099ff')
-	.setTitle('GrahamSH-Bot Help')
-	.setAuthor('GrahamSH-Bot', 'https://uploads.scratch.mit.edu/users/avatars/14542217.png', 'https://grahamsh-llk.github.io')
-	.setDescription('Help for the GrahamSH-Bot.')
-	.addField('g!help', 'Get help', true)
-	.addField('g!say', 'Have the bot say... something!', true)
-	.setImage('https://uploads.scratch.mit.edu/users/avatars/14542217.png')
-	.setTimestamp()
-	.setFooter('Some footer text here', 'https://uploads.scratch.mit.edu/users/avatars/14542217.png');
-  if (message.content.startsWith(prefix) && message.author != '<@777212456170422272>') {
+  console.log(message.content + " -" + message.author);
+  const Help = new Discord.RichEmbed()
+    .setColor("#0099ff")
+    .setTitle("GrahamSH-Bot Help")
+    .setAuthor(
+      "GrahamSH-Bot",
+      "https://uploads.scratch.mit.edu/users/avatars/14542217.png",
+      "https://grahamsh-llk.github.io"
+    )
+    .setDescription("Help for the GrahamSH-Bot.")
+    .addField("g!help", "Get help", true)
+    .addField("g!say", "Have the bot say... something!", true)
+    .setImage("https://uploads.scratch.mit.edu/users/avatars/14542217.png")
+    .setTimestamp()
+    .setFooter(
+      "Some footer text here",
+      "https://uploads.scratch.mit.edu/users/avatars/14542217.png"
+    );
+  if (
+    message.content.startsWith(prefix) &&
+    message.author != "<@777212456170422272>"
+  ) {
     const args = message.content.slice(prefix.length).trim().split(/ +/);
     const command = args.shift().toLowerCase();
-    console.log(args[2])
-    if (command === 'say') {
-        if (args) {
-        message.channel.send("Hahahaha! I'll never say what you want! " + randomSentence({min: 4, max: 9}))
-        }
-      
-    } else if (command === 'help') {
-      message.channel.send(Help)
-    } else if (command === 'ping') {
-      message.channel.send(`🏓 Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
-    } else if (command === 'madlibs'){
+    console.log(args[2]);
+    if (command === "say") {
+      if (args) {
+        message.channel.send(
+          "Hahahaha! I'll never say what you want! " +
+            randomSentence({ min: 4, max: 9 })
+        );
+      }
+    } else if (command === "help") {
+      message.channel.send(Help);
+    } else if (command === "ping") {
+      message.channel.send(
+        `🏓 Latency is ${
+          Date.now() - message.createdTimestamp
+        }ms. API Latency is ${Math.round(client.ws.ping)}ms`
+      );
+    } else if (command === "madlibs") {
       message.channel.send(Sentencer.make(message.content.slice(9)));
-    } else if (command === 'holiday'){
-    message.channel.send(':santa:  Happy holidays, ' + message.author);
-  }
-     else {
-      message.channel.send("Sadly, that's *not* a command.")
+    } else if (command === "holiday") {
+      message.channel.send(":santa:  Happy holidays, " + message.author);
+    } else if (command === "gtg") {
+      message.channel.send(message.author + " has to go!");
     }
-
-
+     else {
+      message.channel.send("Sadly, that's *not* a command.");
+    }
   }
-}
+};
