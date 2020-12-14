25f5fe51721b8f47e3a7714a8d1ba2a103700513
GrahamSH <graham.s.howard@gmail.com>
2020-12-14 19:03:23 +0000

    Changes

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 require("dotenv").config()
 const Discord = require("discord.js")
 const fs = require("fs")
+const jdenticon = require("jdenticon")
 const client = new Discord.Client()
+const { createCanvas, loadImage } = require('canvas')
+process.env.FONTCONFIG_PATH='./fonts';
+
+
 fs.readdir("./events/", (err, files) => {
   files.forEach((file) => {
     const eventHandler = require(`./events/${file}`)
@@ -32,3 +37,38 @@ app.get("/keepalive", (request, response) => {
     console.log('Kept alive!')
     response.send('Alive')
 }); 
+app.get('/picture/:user', async function (req, res, next) {
+  const width = 1200
+  const height = 630
+
+  const canvas = createCanvas(width, height)
+  const context = canvas.getContext('2d')
+
+  context.fillStyle = '#000'
+  context.fillRect(0, 0, width, height)
+
+  context.font = 'bold 70pt'
+  context.textAlign = 'center'
+  context.textBaseline = 'top'
+  context.fillStyle = '#3574d4'
+
+  const text = 'Hello, World!'
+
+  const textWidth = context.measureText(text).width
+  context.fillRect(600 - textWidth / 2 - 10, 170 - 5, textWidth + 20, 120)
+  context.fillStyle = '#fff'
+  context.fillText(text, 600, 170)
+
+  context.font = 'bold 70pt'
+  context.fillStyle = '#fff'
+  context.fillText('flaviocopes.com', 600, 530)
+
+  loadImage('https://grahamshbot.grahamsh.repl.co/picture/' + req.params.user).then(image => {
+    context.drawImage(image, 340, 515, 70, 70)
+    const buffer = canvas.toBuffer('image/png')
+            res.set('Content-Type', 'image/png')
+        res.send(buffer)
+
+  })
+
+    })
